b6b3a4ce1380190617e063b886d9ca0cb08cbf0c
DarkDragonZerox <DarkDragonZerox@users.noreply.github.com>
2020-12-06 19:46:11 -0300

    creacion de recicler en activity main y del fragment para las razas

---
+++ app/src/main/java/cl/evilcorp/perritos/breedFragment.java
@@ -0,0 +1,50 @@
+package cl.evilcorp.perritos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link breedFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class breedFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+   // private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    //private String mParam1;
+    //private String mParam2;
+
+    public breedFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static breedFragment newInstance() {
+        breedFragment fragment = new breedFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_breed, container, false);
+    }
+}
